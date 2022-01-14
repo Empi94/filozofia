33b90335dfc19952c979c0fe8056066f215240b0
Mateusz Pryszmont <mateusz.pryszmont@yahoo.de>
2022-01-14 22:15:45 +0100

    deleted quote.js

--- app/Game.js
+++ app/Game.js
@@ -1,4 +1,32 @@
-import { Quote } from "./Quote.js";
+class Quote {
+  constructor(text) {
+    this.text = text;
+    this.guessed = [];
+  }
+
+  getContent() {
+    let content = "";
+    for (const char of this.text) {
+      if (char == " " || this.guessed.includes(char)) {
+        content += char;
+      } else {
+        content += "_";
+      }
+    }
+
+    return content;
+  }
+
+  guess(letter) {
+    if (!this.text.includes(letter)) {
+      return false;
+    }
+
+    this.guessed.push(letter);
+    return true;
+  }
+}
+
 
 class Game {
   currentStep = 0;
